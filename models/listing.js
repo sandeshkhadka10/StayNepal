63336b2deccefc51017d2a80d34b41e2cb53519b
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-02-10 00:12:51 +0545

    made the model

---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+    title:{
+        type:String
+    },
+    description:{
+        type:String
+    },
+    image:{
+        type:String
+    },
+    price:{
+        type:Number
+    },
+    location:{
+        type:String
+    },
+    country:{
+        type:String
+    }
+});
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+module.exports = Listing;
