b107b7c6d64b7ab25f1e2a31b6e627e60d3c6c45
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-02-09 23:44:45 +0545

    basic setup done

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+
+main()
+    .then(() => {
+        console.log("Connected Successfully");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/bookmenow');
+}
+
+app.listen(8080, () => {
+    console.log("Server is listening to port 8080");
+});
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am root");
+})
