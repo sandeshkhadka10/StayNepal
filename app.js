b8ab56d92bbf09b6e4d169a4aeba52bc672ed074
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-02-11 00:11:34 +0545

    install the ejs-mate

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const methodOverride = require('method-override');
+const ejsMate = require("ejs-mate");
 
 const mongoose = require("mongoose");
 
