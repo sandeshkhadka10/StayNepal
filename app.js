6b9538b5ca4cc976c7f659ae18cabd88a374aee6
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-02-10 23:42:46 +0545

    install and require the necessary package for edit and delete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require('method-override');
 
 const mongoose = require("mongoose");
 
@@ -10,6 +11,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views")); 
 
 app.use(express.urlencoded({extended:true}));
+
+app.use(methodOverride('_method'));
 
 main()
     .then(() => {
