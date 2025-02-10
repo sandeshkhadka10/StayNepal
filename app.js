e3ac6b740092226858be5f8eb0d5f5a9ba4f50f1
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-02-11 00:33:49 +0545

    bug fixed

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const listing = require("./models/listing");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views")); 
 
+app.use(express.static(path.join(__dirname,"public")));
+
 app.use(express.urlencoded({extended:true}));
 
 app.use(methodOverride('_method'));
