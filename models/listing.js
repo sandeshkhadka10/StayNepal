251f17f4002cb0dc6ddc83c1f8739d242648a469
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-02-10 18:56:01 +0545

    changes made in the schema of image

--- models/listing.js
+++ models/listing.js
@@ -10,6 +10,7 @@ const listingSchema = new mongoose.Schema({
     },
     image: {
         type: String,
+        default:"https://images.unsplash.com/photo-1738924349706-14d70715e236?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=https://images.unsplash.com/photo-1540541338287-41700207dee6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D%3D%3D",
         set: (v) => v === "" ? "https://images.unsplash.com/photo-1738924349706-14d70715e236?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=https://images.unsplash.com/photo-1540541338287-41700207dee6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D%3D%3D" : v,
     },
     price: {
