7de7e060f1338c73f610f99e0c340c3a4c7c8c57
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-02-27 21:42:40 +0545

    commented the unnecessary part of the code

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const path = require("path");
 const methodOverride = require('method-override');
 const ejsMate = require("ejs-mate");
 
-const wrapAsync = require("./utils/wrapAsync.js");
+// const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 
-const {listingSchema} = require("./schema.js");
-const {reviewSchema} = require("./schema.js");
+// const {listingSchema} = require("./schema.js");
+// const {reviewSchema} = require("./schema.js");
 
 const mongoose = require("mongoose");
-const listing = require("./models/listing");
-const review = require("./models/review.js");
+// const listing = require("./models/listing");
+// const review = require("./models/review.js");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -38,12 +38,10 @@ app.listen(8080, () => {
     console.log("Server is listening to port 8080");
 });
 
+
 // app.get("/", (req, res) => {
 //     res.send("Hi, I am root");
 // });
-
-
-
 
 app.use("/listing",listings);
 app.use("/listing/:id/review",reviews);
@@ -61,12 +59,6 @@ app.use("/listing/:id/review",reviews);
 //     console.log("sample was saved");
 //     res.send("Successfull test");
 // });
-
-
-
-
-
-
 
 // It is done if somebody gives random url
 app.all("*",(req,res,next)=>{
