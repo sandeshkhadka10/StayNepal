3fd327c35a611b5a1e0d3b17be714afc4359484c
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-08 00:16:07 +0545

    problem in login

--- routes/user.js
+++ routes/user.js
@@ -2,27 +2,34 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
-router.get("/signup",(req,res)=>{
+router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
 
-router.post("/signup",wrapAsync(async(req,res)=>{try{
-    let {username,email,password} = req.body;
-    const newUser = new User({username,email});
-    const registerdUser = await User.register(newUser,password);
-    console.log(registerdUser);
-    req.flash("success","Welcome to BookMeNow");
-    res.redirect("/listing");
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ username, email });
+        const registerdUser = await User.register(newUser, password);
+        console.log(registerdUser);
+        req.flash("success", "Welcome to BookMeNow");
+        res.redirect("/listing");
 
-}catch(e){
-    req.flash("error",e.message);
-    res.redirect("/signup");
-}
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
 }));
 
-router.get("/login",(req,res)=>{
+router.get("/login", (req, res) => {
     res.render("users/login.ejs");
+});
+
+router.post("/login",  passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), async (req, res) => {
+    req.flash("success","Welcome to BookMeNow");
+    res.redirect("/listing");
 });
 
 
