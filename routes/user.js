4b7577926a731735f06d71fb5807994c9625a784
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-07 23:18:42 +0545

    improve the method for showing the error

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
 });
 
-router.post("/signup",async(req,res)=>{
+router.post("/signup",wrapAsync(async(req,res)=>{try{
     let {username,email,password} = req.body;
     const newUser = new User({username,email});
     const registerdUser = await User.register(newUser,password);
     console.log(registerdUser);
     req.flash("success","Welcome to BookMeNow");
     res.redirect("/listing");
-});
+
+}catch(e){
+    req.flash("error","e.message");
+    res.redirect("/signup");
+}
+}));
 
 module.exports = router;
