468dba669b32988ae6aab6e9836d5da8663e766f
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-10 00:19:08 +0545

    added the req.path and req.originalUrl

--- middleware.js
+++ middleware.js
@@ -1,8 +1,10 @@
 module.exports.isLoggedIn = (req,res,next)=>{
     // console.log(req.user);
     // console.log(req);
-    console.log(req.path,"..",req.originalUrl);
+    // console.log(req.path,"..",req.originalUrl);
     if(!req.isAuthenticated()){
+        // login garepachi kaam garne specific path ma jaos bhanera
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error","You must be logged in to create listing!");
         return res.redirect("/login");
     }
