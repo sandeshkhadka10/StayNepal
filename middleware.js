80eb47bc5bdee7a0f1042efffeabdde2214f0233
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-10 23:26:48 +0545

    included the validateReview in the middleware file

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const listing = require("./models/listing");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema,reviewSchema} = require("./schema.js");
 
 module.exports.isLoggedIn = (req,res,next)=>{
     // console.log(req.user);
@@ -35,6 +35,16 @@ module.exports.isOwner = async(req,res,next)=>{
 // It is done for the server side validation
 module.exports.validateListing = (req,res,next)=>{
     let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
+module.exports.validateReview = (req,res,next)=>{
+    let {error} = reviewSchema.validate(req.body);
     if(error){
         let errMsg = error.details.map((el)=> el.message).join(",");
         throw new ExpressError(400,errMsg);
--- routes/review.js
+++ routes/review.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const {reviewSchema} = require("../schema.js");
+// const ExpressError = require("../utils/ExpressError.js");
+// const {reviewSchema} = require("../schema.js");
 const listing = require("../models/listing");
 const review = require("../models/review.js");
+const {validateReview} = require("../middleware.js");
 
-const validateReview = (req,res,next)=>{
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=> el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
+// const validateReview = (req,res,next)=>{
+//     let {error} = reviewSchema.validate(req.body);
+//     if(error){
+//         let errMsg = error.details.map((el)=> el.message).join(",");
+//         throw new ExpressError(400,errMsg);
+//     }else{
+//         next();
+//     }
+// }
 
 // Post Reviews Route
 router.post("/",validateReview,wrapAsync(async(req,res)=>{
