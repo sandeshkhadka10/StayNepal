1aba826d939754875a3d34d7ce13799e5144c23c
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-12 00:05:39 +0545

    router route added

--- routes/listing.js
+++ routes/listing.js
@@ -2,28 +2,37 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const listing = require("../models/listing");
-const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 
+router.route("/")
+    .get(wrapAsync(listingController.indexRoute))
+    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+
+router.route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .patch(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
+
 // Index Route
-router.get("/", wrapAsync(listingController.indexRoute));
+// router.get("/", wrapAsync(listingController.indexRoute));
 
 // New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 // Create Route
-router.post("/", isLoggedIn,validateListing, wrapAsync(listingController.createListing));
+// router.post("/", isLoggedIn,validateListing, wrapAsync(listingController.createListing));
 
 // Edit Route
-router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm));
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
 // Update Route
-router.patch("/:id",isLoggedIn,isOwner,validateListing, wrapAsync(listingController.updateListing));
+// router.patch("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing));
 
 // Delete Route
-router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.deleteListing));
+// router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
 
 // Show Route
-router.get("/:id", wrapAsync(listingController.showListing));
+// router.get("/:id", wrapAsync(listingController.showListing));
 
 module.exports = router;
