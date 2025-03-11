87dc41e97e9b09aa913430efcf911af609e13a3c
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-11 20:38:14 +0545

    removed the unnecessary code

--- routes/listing.js
+++ routes/listing.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-// const ExpressError = require("../utils/ExpressError.js");
-// const {listingSchema} = require("../schema.js");
 const listing = require("../models/listing");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
-
 const listingController = require("../controllers/listing.js");
-
-// It is done for the server side validation
-// const validateListing = (req,res,next)=>{
-//     let {error} = listingSchema.validate(req.body);
-//     if(error){
-//         let errMsg = error.details.map((el)=> el.message).join(",");
-//         throw new ExpressError(400,errMsg);
-//     }else{
-//         next();
-//     }
-// }
 
 // Index Route
 router.get("/", wrapAsync(listingController.index));
