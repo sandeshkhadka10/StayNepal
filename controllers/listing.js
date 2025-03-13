b5ec56640de5a677927022c59ee08fd882003796
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-13 21:42:44 +0545

    added the image in the mongodb

--- controllers/listing.js
+++ controllers/listing.js
@@ -14,9 +14,15 @@ module.exports.renderNewForm = (req, res) => {
 }; 
 
 module.exports.createListing = (async (req,res,next) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    // console.log(url,"..",filename);
+
     const newListing = new listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url,filename};
     await newListing.save();
+
     req.flash("success","New Listing Created!");
     res.redirect("/listing");
 });
