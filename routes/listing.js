373cf13b56b5a595fcc4ddbaa4b70c064bc8e6aa
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-13 19:42:41 +0545

    uploaded the photo to the cloud

--- routes/listing.js
+++ routes/listing.js
@@ -5,7 +5,9 @@ const listing = require("../models/listing");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const {storage} = require("../cloudConfig.js");
+// const upload = multer({ dest: 'uploads/' });
+const upload = multer({ storage});
 
 router.route("/")
     .get(wrapAsync(listingController.indexRoute))
