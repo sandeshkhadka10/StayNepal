d69cfb83d63fbe0827054da63212dc9879e438fe
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-21 00:38:56 +0545

    access the coordinates to the public js file

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,15 @@
 mapboxgl.accessToken = mapToken;
+
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: "mapbox://styles/mapbox/streets-v12",
-    center: [ 85.3222, 27.7103], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
       
+// Create a default Marker and add it to the map.
+console.log(coordinates);
+
+const marker = new mapboxgl.Marker({color:"red"})
+ .setLngLat(coordinates)
+ .addTo(map);
