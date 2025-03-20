47f9b76574636cd815f4421eecf678cf2d8ad7e2
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-21 00:39:49 +0545

    comment added

--- public/js/map.js
+++ public/js/map.js
@@ -8,8 +8,7 @@ const map = new mapboxgl.Map({
 });
       
 // Create a default Marker and add it to the map.
-console.log(coordinates);
-
+// console.log(coordinates); yoh use gareko to check whether coordinates aako cha kinai public file ma
 const marker = new mapboxgl.Marker({color:"red"})
  .setLngLat(coordinates)
  .addTo(map);
