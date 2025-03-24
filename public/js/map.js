4bba4c23e0452187e881ac5fd7af8694e371efcd
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-25 00:43:12 +0545

    added the location in the Popup

--- public/js/map.js
+++ public/js/map.js
@@ -3,12 +3,13 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: "mapbox://styles/mapbox/streets-v12",
-    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
       
 // Create a default Marker and add it to the map.
 // console.log(coordinates);
 const marker = new mapboxgl.Marker({color:"red"})
- .setLngLat(coordinates)
+ .setLngLat(listing.geometry.coordinates)
+ .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h3>${listing.location}</h3><p>Exact Location Provided</p>`))
  .addTo(map);
