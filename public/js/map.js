d80834f8fdea706709cd698c0b38102d15a58eb9
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-24 08:30:44 +0545

    trying to solve the issue that arise while locating the exact location

--- public/js/map.js
+++ public/js/map.js
@@ -3,12 +3,12 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: "mapbox://styles/mapbox/streets-v12",
-    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: [77.209,28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
       
 // Create a default Marker and add it to the map.
-// console.log(coordinates); yoh use gareko to check whether coordinates aako cha kinai public file ma
-const marker = new mapboxgl.Marker({color:"red"})
- .setLngLat(coordinates)
- .addTo(map);
+// console.log(coordinates);
+// const marker = new mapboxgl.Marker({color:"red"})
+//  .setLngLat(coordinates)
+//  .addTo(map);
