723ec0fc1e3bac76a6e4c8f0e1e2c484ac50ae8a
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-03-27 23:53:52 +0545

    changes made to check

--- public/js/map.js
+++ public/js/map.js
@@ -13,3 +13,4 @@ const marker = new mapboxgl.Marker({color:"red"})
  .setLngLat(listing.geometry.coordinates)
  .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h3>${listing.location}</h3><p>Exact Location Provided</p>`))
  .addTo(map);
+ 
