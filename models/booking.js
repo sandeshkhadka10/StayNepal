a6eb83d87552a6a920bd82f5da605fcc3a5b49e4
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-05-18 17:13:22 +0545

    created tthe booking database schema

--- models/booking.js
+++ models/booking.js
@@ -6,5 +6,25 @@ const bookingSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "Listing",
         required: true
+    },
+    name:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    contact:{
+        type:Number,
+        match: /^(98|97)[0-9]{8}$/
+    },
+    checkin:{
+        type:Date
+    },
+    checkout:{
+        type:Date
     }
 });
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+module.exports = Booking;
