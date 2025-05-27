64d34d05c795f3e2eac83921e437d44dcdf1ed87
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-05-27 17:30:10 +0545

    showed the history of booking

--- routes/booking.js
+++ routes/booking.js
@@ -3,14 +3,16 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, validateBooking } = require("../middleware.js");
 const bookingController = require("../controllers/booking.js");
+const booking = require("../models/booking.js");
 
 router.route("/listing/:id/book")
   .get(isLoggedIn,wrapAsync(bookingController.renderBookingForm))
   .post(isLoggedIn,validateBooking,wrapAsync(bookingController.bookingForm))
 
 
-router.get("/listing/:id/bookinghistory",(req,res)=>{
-  res.send("For showing booking history");
+router.get("/listing/:id/bookinghistory",async (req,res)=>{
+  let bookingHistory = await booking.find();
+  res.render("booking/bookingHistory.ejs",{bookingHistory});
 });
 
 module.exports = router;
