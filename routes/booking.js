98daa6573c557d608b23f74ae83549e34bcbec87
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-05-27 16:44:19 +0545

    made the get route

--- routes/booking.js
+++ routes/booking.js
@@ -8,4 +8,9 @@ router.route("/listing/:id/book")
   .get(isLoggedIn,wrapAsync(bookingController.renderBookingForm))
   .post(isLoggedIn,validateBooking,wrapAsync(bookingController.bookingForm))
 
+
+router.get("/listing/:id/bookinghistory",(req,res)=>{
+  res.send("For showing booking history");
+});
+
 module.exports = router;
